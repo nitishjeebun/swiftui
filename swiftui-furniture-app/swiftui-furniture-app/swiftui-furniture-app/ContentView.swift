bcf2d0136f10dd06a21d10b1210ec811c2a5271b
Huy D <minhhuy.uit@gmail.com>
2020-09-10 12:54:31 -0500

    Init swiftui-furniture-app

---
+++ swiftui-furniture-app/swiftui-furniture-app/swiftui-furniture-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-furniture-app
+//
+//  Created by Huy D. on 9/10/20.
+//  Copyright © 2020 Huy D. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-furniture-app/swiftui-furniture-app/swiftui-furniture-appTests/swiftui_furniture_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_furniture_appTests.swift
+//  swiftui-furniture-appTests
+//
+//  Created by Huy D. on 9/10/20.
+//  Copyright © 2020 Huy D. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_furniture_app
+
+class swiftui_furniture_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
