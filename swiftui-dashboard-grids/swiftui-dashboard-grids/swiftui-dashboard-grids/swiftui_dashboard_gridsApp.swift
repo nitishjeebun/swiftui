f84ae6412037a58884bd254ef4d6b6f3ec0cb934
Huy D <minhhuy.uit@gmail.com>
2020-10-06 07:53:31 -0500

    Init swiftui-dashboard-grids

---
+++ swiftui-dashboard-grids/swiftui-dashboard-grids/swiftui-dashboard-grids/swiftui_dashboard_gridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_dashboard_gridsApp.swift
+//  swiftui-dashboard-grids
+//
+//  Created by Huy D. on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_dashboard_gridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
