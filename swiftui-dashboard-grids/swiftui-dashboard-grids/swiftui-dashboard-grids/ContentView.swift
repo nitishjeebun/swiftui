a56a8c369b5cdf9cda9a4abadc2e519bd782360b
Huy D <minhhuy.uit@gmail.com>
2020-10-07 08:39:53 -0500

    Add Tabview & Fitness data.

--- swiftui-dashboard-grids/swiftui-dashboard-grids/swiftui-dashboard-grids/ContentView.swift
+++ swiftui-dashboard-grids/swiftui-dashboard-grids/swiftui-dashboard-grids/ContentView.swift
@@ -9,9 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
+}
+
+struct Home: View {
+    
+    @State var index = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("STAT")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("Color"))
+                Spacer(minLength: 0)
+            }
+            .padding(.horizontal)
+            HStack(spacing: 0) {
+                Text("Day")
+                    .foregroundColor(self.index == 0 ? .white : Color("Color").opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color("Color").opacity(self.index == 0 ? 1: 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        self.index = 0
+                    }
+                Spacer(minLength: 0)
+                Text("Week")
+                    .foregroundColor(self.index == 1 ? .white : Color("Color").opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color("Color").opacity(self.index == 1 ? 1: 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        self.index = 1
+                    }
+                Spacer(minLength: 0)
+                Text("Month")
+                    .foregroundColor(self.index == 2 ? .white : Color("Color").opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color("Color").opacity(self.index == 2 ? 1: 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        self.index = 2
+                    }
+            }
+            .background(Color.black.opacity(0.06))
+            .clipShape(Capsule())
+            .padding(.horizontal)
+            .padding(.top, 25)
+            Spacer(minLength: 0)
+        }
+        .padding(.top)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ swiftui-dashboard-grids/swiftui-dashboard-grids/swiftui-dashboard-grids/Fitness.swift
@@ -0,0 +1,51 @@
+//
+//  Fitness.swift
+//  swiftui-dashboard-grids
+//
+//  Created by Huy D. on 10/7/20.
+//
+
+import Foundation
+
+struct Fitness : Identifiable {
+    var id : Int
+    var title : String
+    var image : String
+    var data: String
+    var suggest : String
+}
+
+// Daily Data...
+
+var fit_Data = [
+    
+    Fitness(id: 0, title: "Heart Rate", image: "heart", data: "124 bpm", suggest: "80-120\nHealthy"),
+
+    Fitness(id: 1, title: "Sleep", image: "sleep", data: "6h 43m", suggest: "Deep Sleep\n5h 13m"),
+    
+    Fitness(id: 2, title: "Energy Burn", image: "energy", data: "583 kcal", suggest: "Daily Goal\n900 kcal"),
+            
+    Fitness(id: 3, title: "Steps", image: "steps", data: "16,741", suggest: "Daily Goal\n20,000 Steps"),
+    
+    Fitness(id: 4, title: "Running", image: "running", data: "5.3 km", suggest: "Daily Goal\n10 km"),
+    
+    Fitness(id: 5, title: "Cycling", image: "cycle", data: "15.3 km", suggest: "Daily Goal\n20 km"),
+]
+
+// Monthly Data...
+
+var week_Fit_Data = [
+
+    Fitness(id: 0, title: "Heart Rate", image: "heart", data: "84 bpm", suggest: "80-120\nHealthy"),
+
+    Fitness(id: 1, title: "Sleep", image: "sleep", data: "43h 23m", suggest: "Deep Sleep\n26h 13m"),
+    
+    Fitness(id: 2, title: "Energy Burn", image: "energy", data: "3,500 kcal", suggest: "Weekly Goal\n4,800 kcal"),
+            
+    Fitness(id: 3, title: "Steps", image: "steps", data: "17,8741", suggest: "Weekly Goal\n25,0000 Steps"),
+    
+    Fitness(id: 4, title: "Running", image: "running", data: "45.3 km", suggest: "Weekly Goal\n70 km"),
+    
+    Fitness(id: 5, title: "Cycling", image: "cycle", data: "100.3 km", suggest: "Weekly Goal\n125 km"),
+]
+
