4036c5de1852af8a40e8ce6fa994f3b56f366c63
Huy D <minhhuy.uit@gmail.com>
2020-10-09 10:27:20 -0500

    Add Assets.

--- swiftui-dating-ui/swiftui-dating-ui/swiftui-dating-ui/ContentView.swift
+++ swiftui-dating-ui/swiftui-dating-ui/swiftui-dating-ui/ContentView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Home()
+    }
+}
+
+struct Home: View {
+    var body: some View {
         Text("Hello, world!")
             .padding()
     }
