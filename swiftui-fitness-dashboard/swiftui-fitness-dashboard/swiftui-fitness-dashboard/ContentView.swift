871f9bc4ee676a6f78e52b5b2f7b37173e066e58
Huy D <minhhuy.uit@gmail.com>
2020-10-12 08:16:55 -0500

    Init swiftui-fitness-dashboard

---
+++ swiftui-fitness-dashboard/swiftui-fitness-dashboard/swiftui-fitness-dashboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-fitness-dashboard
+//
+//  Created by Huy D. on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-fitness-dashboard/swiftui-fitness-dashboard/swiftui-fitness-dashboard/swiftui_fitness_dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_fitness_dashboardApp.swift
+//  swiftui-fitness-dashboard
+//
+//  Created by Huy D. on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_fitness_dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
