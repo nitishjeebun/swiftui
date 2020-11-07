30d4a65eb5c4dec4d10f42bbbb06dbe707bf653b
Huy D <minhhuy.uit@gmail.com>
2020-11-06 22:49:58 -0600

    Init swiftui-hero-animation

---
+++ swiftui-hero-animation/swiftui-hero-animation/swiftui-hero-animation/swiftui_hero_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_hero_animationApp.swift
+//  swiftui-hero-animation
+//
+//  Created by Huy D. on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_hero_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
