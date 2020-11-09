c1d04bd11c34275d0bdf6bd250ada32d859a57bd
Huy D <minhhuy.uit@gmail.com>
2020-11-09 11:56:57 -0600

    Add grid view.

--- swiftui-hero-animation/swiftui-hero-animation/swiftui-hero-animation/ContentView.swift
+++ swiftui-hero-animation/swiftui-hero-animation/swiftui-hero-animation/ContentView.swift
@@ -9,8 +9,100 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
+            .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct Home: View {
+    
+    var columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
+    @State var selected: Travel = data[0]
+    @State var show: Bool = false
+    @Namespace var namespace
+    
+    var body: some View {
+        ZStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                // Top menu
+                HStack {
+                    Text("Travel")
+                        .font(.system(size: 35, weight: .bold))
+                        .foregroundColor(.black)
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image("menu")
+                            .renderingMode(.original)
+                    }
+
+                }
+                .padding([.horizontal, .top])
+                // Grid view
+                LazyVGrid(columns: columns, spacing: 25) {
+                    ForEach(data) { travel in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Image(travel.image)
+                                .resizable()
+                                .frame(height: 180)
+                                .cornerRadius(15)
+                                .onTapGesture {
+                                    withAnimation(.spring()) {
+                                        self.show.toggle()
+                                        self.selected = travel
+                                    }
+                                }
+                                .matchedGeometryEffect(id: travel.id, in: namespace)
+                            Text(travel.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+            
+            // Hero View
+            if self.show {
+                VStack {
+                    ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
+                        Image(self.selected.image)
+                            .resizable()
+                            .frame(height: 300)
+                            .matchedGeometryEffect(id: self.selected.id, in: namespace)
+                        HStack {
+                            Button {
+                                withAnimation(.spring()) {
+                                    self.show.toggle()
+                                }
+                            } label: {
+                                Image(systemName: "xmark")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color.black.opacity(0.5))
+                                    .clipShape(Circle())
+                            }
+                            Spacer()
+                            Button {
+                            } label: {
+                                Image(systemName: "suit.hear.fill")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color.white)
+                                    .clipShape(Circle())
+                            }
+                        }
+                        .padding(.top, 35)
+                        .padding(.horizontal)
+                    }
+                    Spacer()
+                }
+                .background(Color.white)
+            }
+        }
+        .background(Color.white.edgesIgnoringSafeArea(.all))
     }
 }
 
