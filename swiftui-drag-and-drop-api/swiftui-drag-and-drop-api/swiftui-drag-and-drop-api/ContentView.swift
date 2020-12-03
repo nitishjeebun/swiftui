b3a25f92cae9b03701ac821e7fb824eb4ab32a26
Huy D <minhhuy.uit@gmail.com>
2020-12-03 16:23:55 -0600

    Add drag & drop function.

--- swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/ContentView.swift
+++ swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MobileCoreServices
 
 struct ContentView: View {
     var body: some View {
@@ -23,7 +24,8 @@ struct Home: View {
     @ObservedObject var viewModel = ImageViewModel()
     
     var body: some View {
-        VStack(spacing: 15) {
+        VStack {
+            // Image List
             ScrollView {
                 LazyVGrid(columns: self.columns, spacing: 20) {
                     ForEach(self.viewModel.allImages) { image in
@@ -31,10 +33,62 @@ struct Home: View {
                             .resizable()
                             .frame(height: 150)
                             .cornerRadius(15)
+                            .onDrag {
+                                let secureCoding = URL(string: image.imageName)! as NSSecureCoding
+                                let type = String(kUTTypeURL)
+                                return NSItemProvider(item: .some(secureCoding), typeIdentifier: type)
+                            }
                     }
                 }
-                .padding(.all)
+                .padding(.top)
+                .padding(.horizontal)
             }
+            // Drop Area
+            ZStack {
+                // Text
+                if self.viewModel.selectedImages.isEmpty {
+                    Text("Drop Image Here")
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                // Selected Image List
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(self.viewModel.selectedImages, id: \.id) { image in
+                            if !image.imageName.isEmpty {
+                                let alignment = Alignment(horizontal: .trailing, vertical: .top)
+                                ZStack(alignment: alignment) {
+                                    Image(image.imageName)
+                                        .resizable()
+                                        .frame(width: 100, height: 100)
+                                        .cornerRadius(15)
+                                    Button {
+                                        withAnimation(.easeOut) {
+                                            self.viewModel.removeImage(by: image.imageName)
+                                        }
+                                    } label: {
+                                        Image(systemName: "xmark")
+                                            .foregroundColor(.white)
+                                            .padding(10)
+                                            .background(Color.black)
+                                            .clipShape(Circle())
+                                    }
+
+                                }
+                            }
+                        }
+                        Spacer(minLength: 0)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+            .frame(height: self.viewModel.selectedImages.isEmpty ? 100 : nil)
+            .contentShape(Rectangle())
+            .background(Color.black.opacity(0.1))
+            .onDrop(of: [String(kUTTypeURL)], delegate: self.viewModel)
         }
+        .background(Color.black.opacity(0.05))
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
--- swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/ViewModel.swift
+++ swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/swiftui-drag-and-drop-api/ViewModel.swift
@@ -5,9 +5,10 @@
 //  Created by Huy D. on 12/2/20.
 //
 
-import Foundation
+import SwiftUI
+import MobileCoreServices
 
-class ImageViewModel: ObservableObject {
+class ImageViewModel: ObservableObject, DropDelegate {
     
     @Published var allImages = [ImageModel]()
     @Published var selectedImages = [ImageModel]()
@@ -21,6 +22,48 @@ class ImageViewModel: ObservableObject {
             let image = ImageModel(imageName: "\(n)")
             allImages.append(image)
         }
+    }
+    
+    public func removeImage(by imageName: String) {
+        self.selectedImages.removeAll { (check) -> Bool in
+            return check.imageName == imageName
+        }
+    }
+    
+    func performDrop(info: DropInfo) -> Bool {
+        
+        for provider in info.itemProviders(for: [String(kUTTypeURL)]) {
+            
+            guard provider.canLoadObject(ofClass: URL.self) else {
+                print("Can't Load Object")
+                continue
+            }
+            
+            let _ = provider.loadObject(ofClass: URL.self) { (url, error) in
+
+                guard let url = url?.absoluteString else {
+                    print("No Load Object URL")
+                    return
+                }
+                
+                let status = self.selectedImages.contains { (check) -> Bool in
+                    return check.imageName == url
+                }
+                
+                if !status {
+                    DispatchQueue.main.async {
+                        withAnimation(.easeOut) {
+                            let droppedImage = ImageModel(imageName: url)
+                            self.selectedImages.append(droppedImage)
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        
+        return true
     }
     
 }
